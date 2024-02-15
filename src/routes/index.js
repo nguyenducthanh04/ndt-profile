ee96827cb9c6b61a43109bdf34b1f097ddb72d96
Thanh Nguyen <dducthanh04@gmail.com>
2024-02-15 11:01:26 +0700

    update login

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,17 @@ import Blog from "~/pages/Blog";
 import Contact from "~/pages/Contact";
 import Profile from "~/pages/Profile";
 import BlogDetail from "~/pages/BlogDetail";
+import GoogleLogins from "~/pages/GoogleLogins/GoogleLogin";
+import User from "~/pages/users";
+
 const publicRoutes = [
   { path: "/", component: Home },
   { path: "/blog", component: Blog },
   { path: "/contact", component: Contact },
   { path: "/profile", component: Profile },
   { path: "/blog-detail/:id", component: BlogDetail },
+  { path: "/google", component: GoogleLogins },
+  { path: "/user", component: User },
 ];
 const privateRoutes = [];
 export { publicRoutes, privateRoutes };
