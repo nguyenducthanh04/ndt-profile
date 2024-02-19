2e341f3ced9161ce71bc9e18c2cbf041be116ac8
Thanh Nguyen <dducthanh04@gmail.com>
2024-02-19 19:31:51 +0700

    ok

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./Contact.module.scss";
+import axios from "axios";
 const cx = classNames.bind(styles);
 
 function Contact() {
   const [loginUrl, setLoginUrl] = useState("");
+  const [email, setEmail] = useState("");
+  const [content, setContent] = useState("");
+  const [message, setMessage] = useState("");
 
+  //localStorage
+  const userIn = localStorage.getItem("userData");
+  const user = JSON.parse(userIn);
   useEffect(() => {
     fetch("http://localhost:3000/auth/login/url")
       .then((response) => response.json())
@@ -15,12 +22,27 @@ function Contact() {
       })
       .catch((error) => console.error("Error:", error));
   }, []);
-
   const handleLogin = () => {
     window.location.href = loginUrl;
   };
-  const userIn = localStorage.getItem("userData");
-  const user = JSON.parse(userIn);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:3000/mail/send-mail",
+        {
+          email: user.email,
+          content: content,
+        }
+      );
+      setMessage(response.data);
+      alert("Tin nhắn đã được gửi thành công!");
+      window.location.reload();
+    } catch (error) {
+      setMessage("Error sending email");
+    }
+  };
+
   const isLogin = localStorage.getItem("token");
   if (isLogin) {
     return (
@@ -34,9 +56,11 @@ function Contact() {
               <input
                 required=""
                 type="text"
+                name="email"
                 className={cx("input-email")}
                 placeholder=""
                 value={user.email}
+                onChange={(e) => setEmail(e.target.value)}
               ></input>
             </div>
             <div className={cx("textarea-group")}>
@@ -45,13 +69,19 @@ function Contact() {
               <textarea
                 required=""
                 type="text"
+                name="content"
                 className={cx("textarea-email")}
                 placeholder="Bạn muốn nhắn nhủ gì cho tôi ?"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
               ></textarea>
             </div>
             <div className={cx("button-send")}>
-              <button className={cx("btn")}>Gửi</button>
+              <button className={cx("btn")} onClick={handleSubmit}>
+                Gửi
+              </button>
             </div>
+            {message && <p>{message}</p>}
           </div>
         </div>
       </div>
