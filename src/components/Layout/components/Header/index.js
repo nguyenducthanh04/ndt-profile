cb48b37d5a3581a3dfca5b82281c030c8ab75ffc
nguyenducthanh04 <dducthanh04@gmail.com>
2024-06-27 00:53:29 +0700

    update menu mobile

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -61,7 +61,7 @@ function Header() {
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: "100px" }}
+                        // style={{ maxHeight: "100px" }}
                         navbarScroll
                     >
                         <Nav.Link href="/" style={navLink}>
