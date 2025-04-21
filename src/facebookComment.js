41f16d2a4217905d1be05b5a063ea7d0ebfa64f9
unknown <dducthanh04@gmail.com>
2025-04-22 00:39:52 +0700

    update website client

--- src/facebookComment.js
+++ src/facebookComment.js
@@ -6,9 +6,21 @@ const FacebookComment = ({ url }) => {
       window.FB.XFBML.parse(); // làm mới plugin khi component mount
     }
   }, [url]);
-
+  const wrapperStyle = {
+    marginTop: "40px",
+    padding: "16px",
+    backgroundColor: "#f9f9f9",
+    borderRadius: "12px",
+    boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)",
+    maxWidth: "100%",
+    overflowX: "auto",
+    width: "100%",
+    boxSizing: "border-box",
+  };
+  
   return (
     <div
+     style={wrapperStyle}
       className="fb-comments"
       data-href={url}
       data-width="100%"
